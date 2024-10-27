5ae8e2f4fc9e72b6ad8cb2389623921fe7fea76c
jaouad bentahar <jaouad.bentahar@gmail.com>
2024-10-27 17:24:18 +0100

    remove scan since all projects under same name

--- btask-users/src/main/java/com/btask/UserApp.java
+++ btask-users/src/main/java/com/btask/UserApp.java
@@ -2,13 +2,9 @@ package com.btask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ComponentScan(basePackages = {"com.bento", "com.btask"})
-@EnableJpaRepositories(basePackages = {"com.btask.user"})
-@EntityScan(basePackages = {"com.btask.user"})
+@ComponentScan(basePackages = {"com.btask"})
 @SpringBootApplication
 public class UserApp {
 
